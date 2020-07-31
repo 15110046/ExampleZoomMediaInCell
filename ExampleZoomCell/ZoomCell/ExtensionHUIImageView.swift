96afd517b2c44bd919cbf45b0b829f7937095607
NguyenHieu <hieunguyenute@gmail.com>
2020-07-31 11:03:17 +0700

    fix bugs jerking when zoom max remove notifi when zoom refactor code

--- ExampleZoomCell/ZoomCell/ExtensionHUIImageView.swift
+++ ExampleZoomCell/ZoomCell/ExtensionHUIImageView.swift
@@ -7,57 +7,57 @@ import UIKit
 
 extension HImageView {
     
-    func addViewContainer() {
+    public func addViewContainer() {
         guard let window = UIApplication.shared.keyWindow else { return }
         viewContainer = UIView(frame: window.frame)
         guard let viewContainer = viewContainer else { return }
         window.addSubview(viewContainer)
     }
     
-    func viewContainnerAddImageView() {
+    public func viewContainnerAddImageView() {
         imageView = UIImageView(frame: self.globalFrame ?? CGRect())
         imageView?.image = self.image
         guard let imageView = imageView else { return }
         viewContainer?.addSubview(imageView)
     }
     
-    func updateFlags() {
-        frameImageOriginal = imageView!.frame
-        originalImageCenter = imageView!.center
+    public func updateFlags() {
+        guard let imageView = imageView else { return }
+        frameImageOriginal = imageView.frame
+        originalImageCenter = imageView.center
         isZooming = true
-//        flagIsAddSubview = true
     }
     
-    func setupUIWhenStartPinch(scale: CGFloat) {
+    public func setupUIWhenStartPinch(scale: CGFloat) {
         if scale > config.minZoom, viewContainer == nil {
             addViewContainer()
             viewContainnerAddImageView()
             updateFlags()
             self.backgroundColor = .black
             self.image = nil
-            NotificationCenter.default.post(name: NSNotification.Name.CellisZooming, object: nil)
         }
     }
-    func stopZoom() {
+    
+    public func stopZoom() {
         if viewContainer != nil {
             dismiss()
         }
     }
     
-    func reloadUIWhenPinchChanged(sender: UIPinchGestureRecognizer) {
+    public func reloadUIWhenPinchChanged(sender: UIPinchGestureRecognizer) {
         guard let view = imageView else { return }
+        
+        let currentScale = view.frame.size.width / view.bounds.size.width
+        var newScale = currentScale*sender.scale
         
         let pinchCenter = CGPoint(x: sender.location(in: view).x - view.bounds.midX,
                                   y: sender.location(in: view).y - view.bounds.midY)
-        
+                
         let transform = view.transform
                                 .translatedBy(x: pinchCenter.x,  y: pinchCenter.y)
                                 .scaledBy    (x: sender.scale,   y: sender.scale)
                                 .translatedBy(x: -pinchCenter.x, y: -pinchCenter.y)
-        
-        let currentScale = self.imageView!.frame.size.width / self.imageView!.bounds.size.width
-        var newScale = currentScale*sender.scale
-            
+    
         if newScale < config.minZoom {
             if config.autoStopWhenZoomMin {
                 dismiss()
@@ -71,7 +71,10 @@ extension HImageView {
         }
         else {
             if let maxZoom = config.maxZoom, newScale >= maxZoom  {
-                view.transform = CGAffineTransform(scaleX: maxZoom, y: maxZoom)
+                view.transform = view.transform
+                .translatedBy(x: pinchCenter.x,  y: pinchCenter.y)
+                .scaledBy    (x: 1,   y: 1)
+                .translatedBy(x: -pinchCenter.x, y: -pinchCenter.y)
                 sender.scale = 1
             }
             else {
@@ -82,7 +85,7 @@ extension HImageView {
         delegate?.output(scale: newScale, frameImageView: view.frame)
     }
     
-    func acceptNewValueAlpha() {
+    public func acceptNewValueAlpha() {
         let areaImageView = (imageView?.frame.height ?? 0) * (imageView?.frame.width ?? 0)
         let areaImageViewOriginal = frameImageOriginal.height * frameImageOriginal.width
         alphaComponent?(1.2-areaImageViewOriginal/areaImageView)
